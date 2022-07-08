7bc794894f98d606cbe31ac685b651ce341e69bc
Patrick van Everdingen <patrick@passionatepeople.io>
2022-07-08 15:29:42 +0200

    Update Carousel styling, remove HorizontalFeed component

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,10 +1,11 @@
 <template>
   <section>
+    <h2 class="px-4 mb-2 text-2xl">{{ show.genre }}</h2>
     <ul
       class="flex w-full snap-x snap-mandatory gap-4 overflow-x-auto pb-6 before:w-[30vw] after:w-[30vw] after:shrink-0"
     >
       <li
-        v-for="show in showsSortedByGenre[0].shows"
+        v-for="show in show.shows"
         :key="show.id"
         class="relative overflow-hidden shadow-lg rounded-2xl shrink-0 snap-center w-72 h-96"
       >
@@ -21,6 +22,7 @@
 </template>
 
 <script setup>
+import { defineProps } from "vue";
 import { useFetchAllShows } from "@/composables/useFetchAllShows.js";
 import { useGroupByGenre } from "@/composables/useGroupByGenre.js";
 import { useSortByRating } from "@/composables/useSortByRating.js";
@@ -28,7 +30,11 @@ import { useSortByRating } from "@/composables/useSortByRating.js";
 const shows = await useFetchAllShows();
 const showsSortedByGenre = await useGroupByGenre(shows);
 const showsSortedByRating = useSortByRating(shows);
-// console.log(shows);
-// console.log(showsSortedByGenre);
-// console.log(showsSortedByRating);
+
+const props = defineProps({
+  show: {
+    type: Object,
+    required: true,
+  },
+});
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <h1>Home</h1>
-
-  <Carousel />
+  <Carousel v-for="show in showsSortedByGenre" :key="show.id" :show="show" />
 </template>
 
-<script setup></script>
+<script setup>
+import { useFetchAllShows } from "@/composables/useFetchAllShows.js";
+import { useGroupByGenre } from "@/composables/useGroupByGenre.js";
+import { useSortByRating } from "@/composables/useSortByRating.js";
+
+const shows = await useFetchAllShows();
+const showsSortedByGenre = await useGroupByGenre(shows);
+const showsSortedByRating = useSortByRating(shows);
+</script>
