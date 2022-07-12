257148599d7a3e4cff9a581d05bfa54e467b3226
Patrick van Everdingen <patrick@passionatepeople.io>
2022-07-12 17:07:39 +0200

    Add empty and used state

--- components/Input.vue
+++ components/Input.vue
@@ -1,28 +1,38 @@
 <template>
-  <form class="p-4">
+  <form class="p-4" @submit.prevent="submitFormHandler">
     <label
-      for="default-search"
+      for="search"
       class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
       >Search</label
     >
     <div class="relative">
       <div
-        class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
+        class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
       >
-        <IconSearch class="h-5 w-5" />
+        <IconSearch class="w-5 h-5" />
       </div>
       <input
         type="search"
-        id="default-search"
-        class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        name="search"
+        class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
         placeholder="Search TV shows"
         required
+        v-model="tvShowQuery"
       />
       <button
+        v-if="!tvShowQuery"
         type="submit"
-        class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-6 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
       >
-        Search
+        <span>Search</span>
+        <!-- <IconMicrophone class="w-5 h-5" /> -->
+      </button>
+      <button
+        v-else
+        @click="emptyFormHandler"
+        class="text-white absolute right-2.5 bottom-2.5 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+      >
+        <IconCross class="w-5 h-5" />
       </button>
     </div>
   </form>
@@ -31,6 +41,18 @@
 <script setup>
 import { ref } from 'vue';
 
-const username = ref('');
-const formSubmitted = ref(false);
+const tvShowQuery = ref('');
+
+const emptyFormHandler = () => {
+  tvShowQuery.value = '';
+};
+
+const submitFormHandler = () => {
+  router.push({
+    name: 'shows',
+    query: {
+      search: tvShowQuery.value,
+    },
+  });
+};
 </script>
