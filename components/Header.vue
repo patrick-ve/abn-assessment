11b458a8e030e68c6ddb7f3479cad1876d53c36a
Patrick van Everdingen <patrick@passionatepeople.io>
2022-07-12 11:49:48 +0200

    Add header backdrop

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,7 @@
 <template>
-  <header class="w-screen h-20 flex justify-between items-center p-4">
+  <header
+    class="w-screen h-16 flex justify-between items-center p-4 backdrop-blur-sm bg-white/30 fixed left-0 top-0 z-30"
+  >
     <NavbarProfileImage />
     <Logo />
     <IconSearch />
