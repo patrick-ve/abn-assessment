cb55c6657fa1b34756f2d4eabf17d3595cf145c5
Patrick van Everdingen <patrick@passionatepeople.io>
2022-07-12 18:02:14 +0200

    Add styling for results grid

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -2,11 +2,14 @@
   <section class="pt-16">
     <Input @apiCallComplete="showSearchResults" />
     <div v-if="apiCallCompleted && showsFetchedFromApi.length > 0">
-      <ul>
+      <h2 class="pl-4 mb-2 font-extrabold">Results</h2>
+      <ul class="grid grid-cols-2 gap-4 p-4 pt-0">
+        <!-- TODO: Make card height and width dynamic -->
         <Card
           v-for="item in showsFetchedFromApi"
           :key="item.show.id"
           :show="item.show"
+          class="w-full h-full mb-2"
         />
       </ul>
     </div>
