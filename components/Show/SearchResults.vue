ca8d92f3e850348c75f67cd22359453b449c8e20
Patrick van Everdingen <patrick@passionatepeople.io>
2022-07-13 19:39:30 +0200

    Refactor into SearchResults component

---
+++ components/Show/SearchResults.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="apiCallCompleted && showsFetchedFromApi.length > 0">
+    <h2 class="pl-4 mb-2 font-extrabold">
+      Results ({{ showsFetchedFromApi.length }})
+    </h2>
+    <ul class="grid grid-cols-2 gap-4 p-4 pt-0">
+      <!-- TODO: Make card height and width dynamic -->
+      <Card
+        v-for="item in showsFetchedFromApi"
+        :key="item.show.id"
+        :show="item.show"
+        class="w-full h-full mb-2"
+      />
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  showsFetchedFromApi: {
+    type: Array,
+    default: [],
+  },
+  apiCallCompleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
--- pages/search/index.vue
+++ pages/search/index.vue
@@ -5,20 +5,10 @@
       @searchQueryEmptied="emptySearchResults"
       v-autofocus
     />
-    <div v-if="apiCallCompleted && showsFetchedFromApi.length > 0">
-      <h2 class="pl-4 mb-2 font-extrabold">
-        Results ({{ showsFetchedFromApi.length }})
-      </h2>
-      <ul class="grid grid-cols-2 gap-4 p-4 pt-0">
-        <!-- TODO: Make card height and width dynamic -->
-        <Card
-          v-for="item in showsFetchedFromApi"
-          :key="item.show.id"
-          :show="item.show"
-          class="w-full h-full mb-2"
-        />
-      </ul>
-    </div>
+    <ShowSearchResults
+      :apiCallCompleted="apiCallCompleted"
+      :showsFetchedFromApi="showsFetchedFromApi"
+    />
   </section>
 </template>
 
